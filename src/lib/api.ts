ac6ad2574029b2f92958101880e5a39f2680e59d
Tarun <sr.tarun98@gmail.com>
2026-05-06 17:42:02 +0530

    initial

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
-const API_BASE = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_BASE = "https://facechatappbackend.onrender.com/api";
+// const API_BASE = "https://facechatappbackend.onrender.com/api" || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE,
